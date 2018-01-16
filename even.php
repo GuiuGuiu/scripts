e099d6612ca853b524d90f7cbd859fdf236ce762
GuiuGuiu <35487082+GuiuGuiu@users.noreply.github.com>
2018-01-16 11:48:47 +0100

    Add files via upload

---
+++ even.php
@@ -0,0 +1,8 @@
+num = int(input("Intrudueix nombre: "))
+
+if num == 0:
+    print ("Aquest nombre es parell")
+elif num % 2 == 0:
+    print ("Aquest nombre es parell")
+else:
+    print ("Aquest nombre es imparell")
